35c8717812981c2d10adb352aa7e80fa85979ee7
Sarisht shreshth <133949606+sarishtshreshth0@users.noreply.github.com>
2025-02-09 18:57:27 +0530

    app (1).py

--- app (1).py
+++ app (1).py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup as bs
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://sarishtshreshth:rvhs2017@cluster0.sf3lhpf.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://sarishtshreshth:openforall@cluster0.sf3lhpf.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
